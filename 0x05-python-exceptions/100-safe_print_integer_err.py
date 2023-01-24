4b4711a256c9849b8e0a0217a6080cd863cd7eda
Oluwategbe Pelumi <pelumitegbe@gmail.com>
2023-01-24 01:03:47 -0800

    done

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+"""
+Write a function that executes a function safely.
+
+Prototype: def safe_function(fct, *args):
+You can assume fct will be always a pointer to a function
+Returns the result of the function,
+Otherwise, returns None if something happens during the function
+and prints in stderr the error precede by Exception:
+You have to use try: / except:
+"""
+
+
 import sys
+
 
 def safe_print_integer_err(value):
     try:
